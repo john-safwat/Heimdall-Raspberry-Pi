fd8c3adcf9c1c0fa5f9f3a8d33b477550c8474f7
John Safwat <john362sss@gmail.com>
2024-02-26 08:56:00 +0200

    update code

--- Main System/threadingTest.py
+++ Main System/threadingTest.py
@@ -1,13 +1,24 @@
-import datetime
-import time
+import cv2
+from picamera2 import Picamera2
 
+piCam = Picamera2()
+# Set the resolution of the camera
+piCam.preview_configuration.main.size = (1280, 720)
+piCam.preview_configuration.main.format = "RGB888"
+piCam.preview_configuration.align()
+piCam.configure("preview")
+piCam.start()
+while True:
+    print("john")
+    frame = piCam.capture_array()
+    # Convert frame to BGR format
+    print("john")
+    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+    print("john")
+    cv2.imshow("piCam", frame)
+    print("john")
+    # Wait indefinitely for key press to close window
+    if cv2.waitKey(0) == ord('q'):
+        break
 
-
-# Example usage
-time1 = datetime.datetime.now()  # Current time (replace with your actual time)
-time2 = datetime.datetime.now()  # Example time, change as needed
-
-if is_time_difference_2_minutes(time1, time2):
-    print("The time difference is 2 minutes.")
-else:
-    print("The time difference is not 2 minutes.")
+cv2.destroyAllWindows()
